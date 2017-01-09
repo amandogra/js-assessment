7878d4f65591070e3ce7faef70ec8d4ab223391d
Aman Dogra <amandogra@gmail.com>
2017-01-09 12:43:50 -0800

    Completed the `count` test.

--- app/count.js
+++ app/count.js
@@ -2,6 +2,18 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
-
+    var pulse;
+    function countNow() {
+        console.log(start++);
+        if (start <= end) {
+            pulse = setTimeout(countNow, 100);
+        }
+    }
+    countNow();
+    return {
+        cancel : function () {
+            pulse && clearTimeout(pulse);
+        }
+    };
   }
 };
