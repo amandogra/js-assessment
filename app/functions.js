ad24c0987d9f03d3f2ea6815450e904cb315dfbb
Aman Dogra <amandogra@gmail.com>
2017-01-10 15:00:41 -0800

    completed the closures test

--- app/functions.js
+++ app/functions.js
@@ -2,19 +2,34 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.functionsAnswers = {
   argsAsArray: function(fn, arr) {
-
+    return fn.apply(this, arr);
   },
 
   speak: function(fn, obj) {
-
+    return fn.call(obj);
   },
 
   functionFunction: function(str) {
-
+    return function (str1) {
+        return str + ', ' + str1;
+    }
   },
 
   makeClosures: function(arr, fn) {
-
+var funcs = [],
+    item,
+    funcItem = function (a) {
+        return function() {
+            return fn(a);
+        }
+    };
+    console.log(arr);
+    for (var i =0, l=arr.length; i < l; i++) {
+        item = arr[i];
+        funcs.push(funcItem(item));
+    }
+console.log(funcs);
+    return funcs;
   },
 
   partial: function(fn, str1, str2) {
